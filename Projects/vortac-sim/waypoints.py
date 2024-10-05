adf3d0ce6f1db49c67e4d87d3ad70d6a46b9016d
Sami Munir <sami.munir2001@gmail.com>
2024-10-04 21:34:41 -0400

    adding waypoints.py object + get_waypoints() function

---
+++ Projects/vortac-sim/waypoints.py
@@ -0,0 +1,11 @@
+# Define waypoints as a list of (x, y) coordinates.
+waypoints = [
+    (100, 100),
+    (200, 300),
+    (400, 400),
+    (600, 100),
+    (800, 500)
+]
+
+def get_waypoints():
+    return waypoints
