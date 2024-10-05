15645765104684b520798da87404eda29d6f4ab6
Sami Munir <sami.munir2001@gmail.com>
2024-10-04 22:10:22 -0400

    implementing main.py to handle admin control of program + setting up pygame module & screen

--- Projects/vortac-sim/Main.py
+++ Projects/vortac-sim/Main.py
@@ -1,1 +1,9 @@
-print('Hello world')
+import pygame
+from simulation import initialize_aircraft, update_airspace
+from waypoints import get_waypoints
+
+# Initialize pygame module
+pygame.init()
+screen = pygame.display.set_mode((1000, 600))
+pygame.display.set_caption('AI Airspace Simulation')
+font = pygame.font.Font(None, 36)
