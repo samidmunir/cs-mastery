75db12d715f53bbcfe3065ec62fcb90224ec0999
Sami Munir <sami.munir2001@gmail.com>
2024-10-06 02:05:25 -0400

    using trigonometry and conversion from deg -> rad to compute proper triangle orientation (for aircraft) based on heading in deg

--- Projects/vortac-sim/Draw_Utils.py
+++ Projects/vortac-sim/Draw_Utils.py
@@ -1,5 +1,5 @@
-from Main import AIRPORT_ICON_PADDING, AIRPORT_NAME_TEXT_SPACING_LEFT, WAYPOINT_ICON_RADIUS
-import tkinter as tk
+from Main import AIRPORT_ICON_PADDING, AIRPORT_NAME_TEXT_SPACING_LEFT, WAYPOINT_ICON_RADIUS, AIRCRAFT_HEADING_VECTOR_LENGTH
+import math
 import Aircraft
 import Airport
 import Runway
@@ -33,11 +33,45 @@ class Draw_Utils:
 
     def draw_aircraft(self, aircraft: Aircraft.Aircraft):
         # self.canvas.create_image(aircraft.x_loc, aircraft.y_loc, image = self.airplane_icon, anchor = tk.CENTER)
-        points = [aircraft.x_loc, aircraft.y_loc - 10, aircraft.x_loc - 5, aircraft.y_loc + 5, aircraft.x_loc + 5, aircraft.y_loc + 5]
-        print(f'\naircraft.x_loc: {aircraft.x_loc}, aircraft.y_loc: {aircraft.y_loc}')
-        print('points:', points)
-        self.canvas.create_polygon(points, fill = 'blue', outline = 'white', width = 1.5)
+        points = [(aircraft.x_loc, aircraft.y_loc - 10), (aircraft.x_loc - 5, aircraft.y_loc + 5), (aircraft.x_loc + 5, aircraft.y_loc + 5)]
+        # print(f'\naircraft.x_loc: {aircraft.x_loc}, aircraft.y_loc: {aircraft.y_loc}')
+        # print('points:', points)
+
+        angle_rad = math.radians(aircraft.heading)
+
+        rotated_points = []
+        for px, py in points:
+            new_x, new_y = self.rotate_points(px, py, aircraft.x_loc, aircraft.y_loc, angle_rad)
+            rotated_points.append((new_x, new_y))
+
+        self.canvas.create_polygon(rotated_points, fill = '#0000ff', outline = '#ffffff', width = 1.5)
         
         info = f'{aircraft.flight_number} {aircraft.ac_type}\n{aircraft.speed}kts {aircraft.altitude}ft\n{aircraft.heading}°'
         
         self.canvas.create_text(aircraft.x_loc + 60, aircraft.y_loc - 20, text = info, fill = '#ffffff', font = ('Helvetica', 10))
+        # self.draw_aircraft_heading_vector(aircraft)
+    
+    def draw_aircraft_heading_vector(self, aircraft: Aircraft.Aircraft):
+        a = aircraft.x_loc + AIRCRAFT_HEADING_VECTOR_LENGTH * math.cos(math.radians(aircraft.heading + 90.0))
+        b = aircraft.y_loc - AIRCRAFT_HEADING_VECTOR_LENGTH * math.sin(math.radians(aircraft.heading + 90.0))
+
+        # self.canvas.create_line(aircraft.x_loc, aircraft.y_loc, aircraft.x_loc, aircraft.y_loc - AIRCRAFT_HEADING_VECTOR_LENGTH, fill = '#ffffff', width = 1.5)
+        self.canvas.create_line(aircraft.x_loc, aircraft.y_loc, a, b, fill = '#ffffff', width = 1.5)
+    
+    def rotate_points(self, x, y, cx, cy, angle_rad):
+        cos_theta = math.cos(angle_rad)
+        sin_theta = math.sin(angle_rad)
+
+        x -= cx
+        y -= cy
+
+        x_new = x * cos_theta - y * sin_theta
+        y_new = x * sin_theta + y * cos_theta
+
+        x_rotated = x_new + cx
+        y_rotated = y_new + cy
+
+        return x_rotated, y_rotated
+
+    def draw_terrain_polygon(self, points):
+        self.canvas.create_polygon(points, fill = '#04e100', outline = '#ff0000', width = 1.5)
--- Projects/vortac-sim/Main.py
+++ Projects/vortac-sim/Main.py
@@ -11,6 +11,7 @@ AIRPORT_ICON_PADDING = 10.0 # units in px
 AIRPORT_NAME_TEXT_SPACING_LEFT = 40.0 # units in px
 RUNWAY_LENGTH_SCALAR = 0.010 # Adjust this value to scale the runway length.
 WAYPOINT_ICON_RADIUS = 5.0 # units in px
+AIRCRAFT_HEADING_VECTOR_LENGTH = 40.0 # units in px
 
 class Main:
     def __init__(self, root):
@@ -43,9 +44,33 @@ class Main:
         # Draw each waypoint onto the VORTAC canvas.
         for waypoint in WAYPOINTS_LIST:
             DU.Draw_Utils(self.canvas).draw_waypoint(waypoint)
+
+
+        # Create a list of aircrafts as (name, ac_type, x_loc, y_loc, altitude, speed, heading)
+        aircrafts = [
+            ('SDM147', 'B789', 900.0, 450.0, 17000, 265, 360.0),
+            ('UAL225', 'B38M', 320.0, 325.0, 15000, 250, 180.0),
+            ('DAL1738', 'A20N', 250.0, 300.0, 21000, 300, 135.0),
+            ('SYMA5', 'A380', 925.0, 185.0, 5000, 275, 270.0)
+        ]
+        AIRCRAFTS_LIST = []
+        for aircraft in aircrafts:
+            new_aircraft = Aircraft.Aircraft(aircraft[0], aircraft[1], aircraft[2], aircraft[3], aircraft[4], aircraft[5], aircraft[6])
+            AIRCRAFTS_LIST.append(new_aircraft)
+        # Draw each aircraft onto the VORTAC canvas.
+        for aircraft in AIRCRAFTS_LIST:
+            DU.Draw_Utils(self.canvas).draw_aircraft(aircraft)
         
-        aircraft = self.create_aircraft('SDM147', 'B789', 900.0, 450.0, 17000, 265, 360.0)
-        DU.Draw_Utils(self.canvas).draw_aircraft(aircraft)
+        # aircraft = self.create_aircraft('SDM147', 'B789', 900.0, 450.0, 17000, 265, 360.0)
+        # DU.Draw_Utils(self.canvas).draw_aircraft(aircraft)
+
+        # Create points for terrain regions on VORTAC canvas.
+        terrain_1_points = [(100, 100), (200, 200), (300, 100)]
+        terrain_2_points = [(500, 300), (600, 400), (400, 400)]
+        terrain_3_points = [(700, 500), (800, 600), (600, 600)]
+        DU.Draw_Utils(self.canvas).draw_terrain_polygon(terrain_1_points)
+        DU.Draw_Utils(self.canvas).draw_terrain_polygon(terrain_2_points)
+        DU.Draw_Utils(self.canvas).draw_terrain_polygon(terrain_3_points)
 
     def create_airport(self) -> Airport:
         name: str = 'NX77'
